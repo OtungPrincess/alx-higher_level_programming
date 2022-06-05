3339c576d7bde6cf98e4615eb4ccd3c6d50e9ffe
Otung Princess <otungprincess@gmail.com>
2022-06-05 05:16:08 -0700

    tuples

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,6 +1,5 @@
 #!/bin/usr/python3
 def element_at(my_list, idx):
-    if (idx < 0 and idx > len(my_list)):
+    if (idx < 0 and idx >= len(my_list)):
         return None
-    else:
-        return (my_list[idx])
+    return (my_list[idx])
