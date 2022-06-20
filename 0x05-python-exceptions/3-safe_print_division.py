203aa1df79b7738f418c6316d0aaeafb03dc5620
Otung Princess <otungprincess@gmail.com>
2022-06-20 09:54:12 -0700

    exception

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    try:
+        divide = a / b
+    except (ZeroDivisionError, FloatingPointError):
+        divide = None
+    except TypeError:
+        divide = None
+    finally:
+        print('Inside result: {}'.format(divide))
+    return divide
