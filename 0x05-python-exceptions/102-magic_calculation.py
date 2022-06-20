d21a91e6b86e9929b90160e72e53123bb09d6e01
Otung Princess <otungprincess@gmail.com>
2022-06-20 10:52:48 -0700

    python

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -7,7 +7,7 @@ def magic_calculation(a, b):
                 raise Exception('Too far')
             else:
                 result += a ** b / n
-        except:
+        except Exception:
             result = b + a
             break
     return result
