25e9882183c1b1b4b93d2de6599c8cf374080dff
Otung Princess <otungprincess@gmail.com>
2022-09-23 21:21:00 -0700

    python script

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -4,8 +4,8 @@
 import requests
 
 if __name__ == "__main__":
-    with requests.get('https://intranet.hbtn.io/status') as resp:
-        body = resp.text
-        print("Body response:")
-        print("\t- type: {}".format(type(body)))
-        print("\t- content: {}".format(body))
+    response = requests.get('https://intranet.hbtn.io/status')
+    body = response.text
+    print("Body response:")
+    print("\t- type: {}".format(type(body)))
+    print("\t- content: {}".format(body))
