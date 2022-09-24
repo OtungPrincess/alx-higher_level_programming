83a5be2ab0bef17dffb0ba1205692536cacdd00c
Otung Princess <otungprincess@gmail.com>
2022-09-23 20:23:47 -0700

    output error corrected

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -9,6 +9,6 @@ if __name__ == "__main__":
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as resp:
         body = resp.read()
         print('Body response:')
-        print('\t- type: {}'.format(type(body)))
-        print('\t- content: {}'.format(body))
-        print('\t- utf8 content: {}'.format(body.decode("utf-8")))
+        print('    - type: {}'.format(type(body)))
+        print('    - content: {}'.format(body))
+        print('    - utf8 content: {}'.format(body.decode("utf-8")))
