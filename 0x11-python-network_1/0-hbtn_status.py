b94919f7402a5bb5df6191df1fef495586946c09
Otung Princess <otungprincess@gmail.com>
2022-09-23 20:11:32 -0700

    python script

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 """Fetches https://intranet.hbtn.io/status"""
 
+import urllib.request
 
 if __name__ == "__main__":
-    import urllib.request
 
+    request = urllib.request.Request("https://alx-intranet.hbtn.io/status")
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as resp:
-        html = resp.read()
+        body = resp.read()
         print('Body response:')
-        print('\t- type: {}'.format(type(html)))
-        print('\t- content: {}'.format(html))
-        print('\t- utf8 content: {}'.format(html.decode("utf-8")))
+        print('\t- type: {}'.format(type(body)))
+        print('\t- content: {}'.format(body))
+        print('\t- utf8 content: {}'.format(body.decode("utf-8")))
--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -6,7 +6,5 @@ import requests
 if __name__ == "__main__":
 
     req = requests.get('https://intranet.hbtn.io/status')
-
-    print('Body response:')
-    print('\t- type: {}'.format(type(req.text)))
-    print('\t- content: {}'.format(req.text))
+    t = req.text
+    print('Body response:\n\t- type: {}\n\t- content: {}'.format(type(t), t))
