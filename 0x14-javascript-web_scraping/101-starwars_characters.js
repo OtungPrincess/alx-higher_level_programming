d40ca9caaf56b70cadeaf5847e0bb45ba3bad221
Princess <otungprincess@gmail.com>
2022-10-18 15:19:02 +0000

    starwars

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,24 +1,27 @@
 #!/usr/bin/node
-
 const request = require('request');
-
-function helpRequest (arr, i) {
-  if (i === arr.length) {
-    return;
-  }
-  request(arr[i], function (error, response, body) {
-    if (error) {
-      console.error(error);
+const film = process.argv[2];
+let url = 'http://swapi.co/api/people/';
+function filmcharacters (film, url) {
+  request(url, function (err, response, body) {
+    if (err) {
+      console.log(err);
+    } else if (response.statusCode === 200) {
+      let jsonobj = JSON.parse(body);
+      let people = jsonobj.results;
+      for (let i in people) {
+        for (let j in people[i].films) {
+          if (people[i].films[j].includes(film)) {
+            console.log(people[i].name);
+          }
+        }
+      }
+      if (jsonobj.next !== null) {
+        filmcharacters(film, jsonobj.next);
+      }
+    } else {
+      console.log('An error occured. Status code: ' + response.statusCode);
     }
-    console.log(JSON.parse(body).name);
-    helpRequest(arr, i + 1);
   });
 }
-
-request('http://swapi.co/api/films/' + process.argv[2], function (error, response, body) {
-  if (error) {
-    console.error(error);
-  }
-  const charac = JSON.parse(body).characters;
-  helpRequest(charac, 0);
-});
+filmcharacters(film, url);
