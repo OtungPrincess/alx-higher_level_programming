ae74dd5f1a6dc23a750165ce12384815b12d6037
Princess <otungprincess@gmail.com>
2022-10-18 14:50:18 +0000

    completed tasks

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -2,8 +2,8 @@
 
 const request = require('request');
 
-Url = 'https://swapi-api.hbtn.io/api/films/';
-request(Url + process.argv[2], function (error, response, body) {
+Url = 'https://swapi-api.hbtn.io/api';
+request(Url + '/films/' + process.argv[2], function (error, response, body) {
   if (error) {
     console.error(error);
   }
